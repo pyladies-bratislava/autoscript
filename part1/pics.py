9fffe81c38ca97d8832d6a0df34346b3f9ad3bc3
Filipa Andrade <fandrade@laterpay.net>
2024-03-04 16:07:39 +0100

    Improve part1 script

--- part1/pics.py
+++ part1/pics.py
@@ -1,40 +1,37 @@
-# Organize pictures by year/month according to the filename
 import os
 import shutil
 
 
+# Organize pictures by year/month according to the filename
 def organize_pics(folder):
 
-    print(f"Organizing pictures in {folder}...")
+    print(f"Organizing photos in {folder} folder...")
 
+    # For each file in the folder
     for filename in os.listdir(folder):
-        # Print the filename
+
+        # Step 1 - Get information from the filename
         print(f"Processing {filename}")
+        filepath = os.path.join(folder, filename)
+        year = filename[0:4]  # String slicing by index 0 1 2 3
+        month = filename[4:6]  # String slicing by index 4 5
+        print(f"Path: {filepath} Year: {year}, Month: {month}")
 
-        # Get information from the filename
-        path = os.path.join(folder, filename)
-        year = filename[:4]  # String slicing by index
-        month = filename[4:6]
-        print(f"Path: {path}: Year: {year}, Month: {month}")
-
-        # Define folder's name, e.g. 2023/05
+        # Step 2 - Define folder's name, e.g. 2023/05
         new_folder = os.path.join("organized", year, month)
         print(f"New folder: {new_folder}")
 
-        # Create the folder if it doesn't exist
+        # Step 3 - Create a folder if it doesn't exist
         if not os.path.exists(new_folder):
             os.makedirs(new_folder)
+            print(f"Created new folder: {new_folder}")
 
-        # Copy the file to the new folder (keeps the original)
-        shutil.copy(path, os.path.join(new_folder, filename))
+        # Step 4 - Copy the file to the appropriate folder (keeps the original)
+        new_filepath = os.path.join(new_folder, filename)
+        shutil.copy(filepath, new_filepath)
         print(f"File {filename} copied to {new_folder}")
-
-        # Move the file to the new folder (deletes the original)
-        # shutil.move(pic_path, os.path.join(pic_new_folder, pic_name))
 
     print(f"Organizing pictures in {folder} completed.")
 
 
-# Entry point of the script/program
-if __name__ == "__main__":
-    organize_pics("pictures")
+organize_pics("pictures")
