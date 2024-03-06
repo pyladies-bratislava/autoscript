8c010bdf716c5b8181f3c638d0b6d42b29450fe9
Filipa Andrade <fandrade@laterpay.net>
2024-03-06 18:15:20 +0100

    Format with black

--- gui.py
+++ gui.py
@@ -6,8 +6,6 @@ from sort import organize_pics
 
 # Make a simple GUI application that organizes pictures by year/month according to the filename.
 def photos_app():
-    folder = None
-
     window = tk.Tk()
     window.title("Sort pictures")
     window.geometry("400x300")
@@ -17,14 +15,20 @@ def photos_app():
 
     # Chose a folder to organize
     label = tk.Label(text="Select a folder with photos:")
-    label.pack(side='left')
+    label.pack(side="left")
     button1 = tk.Button(window, text="Select", width=15, height=2)
     button1.bind("<Button-1>", handle_click)
-    button1.pack(side='right', padx=2)
+    button1.pack(side="right", padx=2)
 
     label = tk.Label(text="Organize pictures!")
-    label.pack(side='right', padx=1)
-    button2 = tk.Button(window, text="Click here to organize pictures", width=25, height=2, command=lambda: organize_pics(window.folder))
-    button2.pack(side='left', padx=2)
+    label.pack(side="right", padx=1)
+    button2 = tk.Button(
+        window,
+        text="Click here to organize pictures",
+        width=25,
+        height=2,
+        command=lambda: organize_pics(window.folder),
+    )
+    button2.pack(side="left", padx=2)
 
     window.mainloop()
--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 from gui import photos_app
-
+from sort import organize_pics
 
 # Part 1 - Organize photos by filename
-# organize_pics("photos")
+organize_pics("photos")
 
 # Part 2 - GUI for organizing photos
 photos_app()
