8d51f7e5d741a45989df57c5166c8def480b3bf2
Filipa Andrade <fandrade@laterpay.net>
2024-03-15 14:26:28 +0100

    Change the script to sort only by month

--- sort.py
+++ sort.py
@@ -16,11 +16,11 @@ def sort_photos(folder):
 
         # Step 1 - Get information from the filename
         filepath = os.path.join(folder, filename)
-        year = filename[0:4]  # String slicing by index 0 1 2 3
-        print(f"Step 1 - Path: {filepath} Year: {year}")
+        month = filename[4:6]  # String slicing by index 4 5
+        print(f"Step 1 - Path: {filepath} Month: {month}")
 
-        # Step 2 - Define folder's name, e.g. 2023
-        new_folder = os.path.join("newname", year)
+        # Step 2 - Define folder's name, e.g. 2023/05
+        new_folder = os.path.join("newname", month)
         print(f"Step 2 - New folder: {new_folder}")
 
         # Step 3 - Create a folder if it doesn't exist
