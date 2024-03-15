71ac4bf79d5728433b19062587163785b44e1a3d
Filipa Andrade <fandrade@laterpay.net>
2024-03-15 14:22:44 +0100

    Print the number of the step

--- sort.py
+++ sort.py
@@ -10,27 +10,27 @@ def sort_photos(folder):
     for filename in os.listdir(folder):
 
         # Step 0 - Print the filename
-        print(f"Processing {filename}")
+        print(f"Step 0 - Processing {filename}")
 
         # Step 1 - Get information from the filename
         filepath = os.path.join(folder, filename)
         year = filename[0:4]  # String slicing by index 0 1 2 3
         month = filename[4:6]  # String slicing by index 4 5
-        print(f"Path: {filepath} Year: {year}, Month: {month}")
+        print(f"Step 1 - Path: {filepath} Year: {year}, Month: {month}")
 
         # Step 2 - Define folder's name, e.g. 2023/05
         new_folder = os.path.join("newname", year, month)
-        print(f"New folder: {new_folder}")
+        print(f"Step 2 - New folder: {new_folder}")
 
         # Step 3 - Create a folder if it doesn't exist
         if not os.path.exists(new_folder):
             os.makedirs(new_folder)
-            print(f"Created new folder: {new_folder}")
+            print(f"Step 3 - Created new folder: {new_folder}")
 
         # Step 4 - Copy the file to the appropriate folder
         new_filepath = os.path.join(new_folder, filename)
         shutil.copy(filepath, new_filepath)
-        print(f"File {filename} copied to {new_folder}")
+        print(f"Step 4 - File {filename} copied to {new_folder}")
 
         print("------")
 
